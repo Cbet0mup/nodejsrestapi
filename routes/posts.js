9e64f1e25e8bc7eaee2629645349eccfe0cfb67c
Lyutov Pavel <cbet0mup.pro@gmail.com>
2020-07-09 00:34:56 +0700

    add any func

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const Post = require('../models/post')
+const Post = require('../models/post');
+const { syncIndexes } = require('../models/post');
 const router = express.Router();
 
 // Получаем все записи
@@ -11,6 +12,17 @@ router.get('/', async(req, res) => {
         res.json({message: err});
     }
 });
+
+// Получаем запись из БД по ID
+router.get('/:postID', async(req, res) => {
+    try{
+       const post = await Post.findById(req.params.postID);
+        res.json(post);
+        console.log(req.params.postID);
+    }catch(err){
+        res.json({message: err});
+    }
+})
 
 // отправляем запись в БД согласно заданной модели
 router.post('/', async(req, res) => {
@@ -24,9 +36,33 @@ router.post('/', async(req, res) => {
     res.json(savedPost);
     } catch(err){res.json({message: err})};
 });
-// router.get('/specific', (req, res) => {
-//     res.send("SPECIFIC POST");
-// });
+
+//REMOVE
+router.delete('/:postID', async(req, res) => {
+    try {
+        const removedPost = await Post.deleteOne({_id: req.params.postID});
+        console.log('Deleted: ' + res.body.postID);
+
+        res.json(removedPost);
+
+    } catch (error) {
+        res.json({message: error});
+    }
+});
+
+//UPDATE BD
+
+router.patch('/:postID', async(req, res) => {
+    try {
+        const updatePost = await Post.updateOne(
+            {_id: req.params.postID},
+            {$set: {title: req.body.title}}
+            );
+            res.json(updatePost);
+    } catch (error) {
+        res.json({message: error});
+    }
+});
 
 
 
