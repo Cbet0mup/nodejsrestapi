5a4320fb3790736c10a5fa57c74ced751d1a0e47
Lyutov Pavel <cbet0mup.pro@gmail.com>
2020-07-17 01:07:21 +0700

    add fetch and parse singleUser data

--- source/index.js
+++ source/index.js
@@ -1,12 +1,44 @@
-const myForm = document.getElementById('myForm');
-const input = document.getElementById('username');
-const output = document.getElementById('output');
 
-myForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    //let text = input.
-    console.log("Form has been submitted!")
-    console.log(input.value)
-    output.innerText = input.value;
-    input.value = "";
-})
+const myForm = document.getElementById('myForm');
+// const input = document.getElementById('username');
+const output = document.getElementById('response');
+
+const getUserUrl = "https://reqres.in/api/users/2"
+
+ myForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        getResource(getUserUrl)
+            .then(data => getSingleUser(data))
+            .catch((err)=>{console.log(err)});
+     });
+
+
+async function getResource(url){
+
+    const res = await fetch(`${url}`);
+
+    if(!res.ok){
+        throw new Error(`Could notfetch ${url}, status: ${res.status}`);
+    } else {
+        return await res.json(); 
+    }
+}
+
+function getSingleUser(response){
+
+    let obj = response.data;
+    //console.log(obj)
+    for (const key in obj) {
+        console.log(key + " : " + obj[key])
+    }
+
+    output.innerHTML = `
+        <img src="${obj.avatar}" alt="">
+        <p class="text">${obj.first_name + " " + obj.last_name}</p>
+        <p class="text">${obj.email}</p>
+    `;
+}
+
+
+    
