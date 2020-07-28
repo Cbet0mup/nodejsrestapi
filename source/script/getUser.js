f3277e17685b2e62aec3461047c32cb4d6e5dcd6
Lyutov Pavel <cbet0mup.pro@gmail.com>
2020-07-29 01:26:38 +0700

    table add

--- source/script/getUser.js
+++ source/script/getUser.js
@@ -1,23 +1,45 @@
 function getUser(data){
     
     resBlockInit();
+    getTable();
+    let tbody = document.getElementById('tbody');
     
     data.forEach(element => {
-        let ul = document.createElement('ul'); 
-        ul.id = 'ul';
-        let blockName = document.createElement('li');
-        let blockPost = document.createElement('li');
-        let blockEmail = document.createElement('li');
-        
-        blockName.innerHTML = 'Name: ' + `${element.name}`;
-        blockPost.innerHTML = 'Post: ' + `${element.post}`;
-        blockEmail.innerHTML = 'Email: ' + `${element.email}`;
+        const tr = document.createElement('tr');
+        let th1 = document.createElement('td');
+        let th2 = document.createElement('td');
+        let th3 = document.createElement('td');
 
-        ul.append(blockName);
-        ul.append(blockPost);
-        ul.append(blockEmail);
-        output.append(ul);
+        th1.innerHTML = `${element.name}`;
+        th2.innerHTML = `${element.post}`;
+        th3.innerHTML = `${element.email}`;
+
+        tr.appendChild(th1);
+        tr.appendChild(th2);
+        tr.appendChild(th3);
+
+
+        tbody.appendChild(tr);
+
+
+
+
+        // let ul = document.createElement('ul'); 
+        // ul.id = 'ul';
+        // let blockName = document.createElement('li');
+        // let blockPost = document.createElement('li');
+        // let blockEmail = document.createElement('li');
+        
+        // blockName.innerHTML = 'Name: ' + `${element.name}`;
+        // blockPost.innerHTML = 'Post: ' + `${element.post}`;
+        // blockEmail.innerHTML = 'Email: ' + `${element.email}`;
+
+        // ul.append(blockName);
+        // ul.append(blockPost);
+        // ul.append(blockEmail);
+        // output.append(ul);
         
     });
 
 }
+
---
+++ source/script/table.js
@@ -0,0 +1,31 @@
+function getTable(){
+    const table = document.createElement('table');
+    table.classList.add('table');
+    table.classList.add('table-striped');
+    table.classList.add('table-dark');
+    table.classList.add('table-hover');
+
+
+
+    const thead = document.createElement('thead');
+    const tr = document.createElement('tr');
+    tr.classList.add('table-active');
+
+    const th = document.createElement('th');
+    th.innerHTML = 'Имя';
+    tr.appendChild(th);
+    const th2 = document.createElement('th');
+    th2.innerHTML = 'Должность';
+    tr.appendChild(th2);
+    const th3 = document.createElement('th');
+    th3.innerHTML = 'Email';
+    tr.appendChild(th3);
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    const tbody = document.createElement('tbody');
+    tbody.id = 'tbody';
+    table.appendChild(tbody);
+
+    output.appendChild(table);
+}
