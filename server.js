ab4c5aea6a86a002ddb130921cbe06cc7bf6b512
Lyutov Pavel <cbet0mup.pro@gmail.com>
2020-08-15 03:20:54 +0700

    push6

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use('/posts', postRoute);
 
 app.use('/', express.static('source'));  // чтобы не ебаться с шаблонизаторами
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80; //3000
 
 //DB connect 
 
